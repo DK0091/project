c817b14103f8eb78cc94afd0ee7e79bb3fb1af71
Darshan Kale <kaledarshan48@gmail.com>
2025-09-06 23:38:29 +0530

    connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello")
+import dotenv from "dotenv"
+import connectdb from "./db/index.js";
+import app from "./app.js"
+dotenv.config({path:'./.env'
+});
+
+connectdb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+         console.log(` Server running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("Error occured",error)
+})
+
+/*
+import express from 'express'
+const app=express();
+
+;(async()=>{
+try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+
+} catch (error) {
+    console.error("Error",error)
+}
+})();
+
+*/
